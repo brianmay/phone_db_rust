cc4228054e52efa1282228fd6793943548761866
Brian May <brian@linuxpenguins.xyz>
2024-12-14 08:42:29 +1100

    feat: add script to load secrets

---
+++ secrets.sh
@@ -0,0 +1,8 @@
+export OIDC_DISCOVERY_URL
+export OIDC_CLIENT_ID
+export OIDC_CLIENT_SECRET
+export OIDC_AUTH_SCOPE
+OIDC_DISCOVERY_URL="$(pass oidc/discovery_url)"
+OIDC_CLIENT_ID="$(pass show oidc/client_id)"
+OIDC_CLIENT_SECRET="$(pass show oidc/client_secret)"
+OIDC_AUTH_SCOPE="openid profile groups email"
