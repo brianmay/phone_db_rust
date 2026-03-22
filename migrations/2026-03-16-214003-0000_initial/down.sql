b3151a8b00fd429c0800534e57015c8926caf5d1
Brian May <brian@linuxpenguins.xyz>
2026-03-22 17:56:32 +1100

    fix: update initial database from prod

--- migrations/2026-03-16-214003-0000_initial/down.sql
+++ migrations/2026-03-16-214003-0000_initial/down.sql
@@ -4,3 +4,5 @@ DROP TABLE IF EXISTS "defaults";
 DROP TABLE IF EXISTS "phone_calls";
 DROP TABLE IF EXISTS "contacts";
 DROP TABLE IF EXISTS "schema_migrations";
+DROP INDEX IF EXISTS users_username_index;
+DROP INDEX IF EXISTS contacts_phone_number_index;
--- migrations/2026-03-16-214003-0000_initial/up.sql
+++ migrations/2026-03-16-214003-0000_initial/up.sql
@@ -1,16 +1,16 @@
 -- Your SQL goes here
 CREATE TABLE "users"(
-	"id" INT8 NOT NULL PRIMARY KEY,
+	"id" BIGSERIAL PRIMARY KEY,
 	"username" VARCHAR(255),
 	"password_hash" VARCHAR(255),
-	"is_admin" BOOL,
-	"is_trusted" BOOL,
-	"is_phone" BOOL,
+	"is_admin" BOOL DEFAULT false,
+	"is_trusted" BOOL DEFAULT false,
+	"is_phone" BOOL DEFAULT false,
 	"inserted_at" TIMESTAMPTZ NOT NULL,
 	"updated_at" TIMESTAMPTZ NOT NULL
 );
 CREATE TABLE "defaults"(
-	"id" INT8 NOT NULL PRIMARY KEY,
+	"id" BIGSERIAL PRIMARY KEY,
 	"order" INT4,
 	"regexp" VARCHAR(255),
 	"name" VARCHAR(255),
@@ -19,7 +19,7 @@ CREATE TABLE "defaults"(
 	"updated_at" TIMESTAMPTZ NOT NULL
 );
 CREATE TABLE "contacts"(
-	"id" INT8 NOT NULL PRIMARY KEY,
+	"id" BIGSERIAL PRIMARY KEY,
 	"phone_number" VARCHAR(255) NOT NULL,
 	"name" VARCHAR(255),
 	"action" VARCHAR(255) NOT NULL,
@@ -28,7 +28,7 @@ CREATE TABLE "contacts"(
 	"comments" VARCHAR(255)
 );
 CREATE TABLE "phone_calls"(
-	"id" INT8 NOT NULL PRIMARY KEY,
+	"id" BIGSERIAL PRIMARY KEY,
 	"action" VARCHAR(255) NOT NULL,
 	"contact_id" INT8 NOT NULL,
 	"inserted_at" TIMESTAMPTZ NOT NULL,
@@ -40,3 +40,5 @@ CREATE TABLE "schema_migrations"(
 	"version" INT8 NOT NULL PRIMARY KEY,
 	"inserted_at" TIMESTAMP
 );
+CREATE UNIQUE INDEX users_username_index ON users USING btree (username);
+CREATE UNIQUE INDEX contacts_phone_number_index ON contacts USING btree (phone_number);
